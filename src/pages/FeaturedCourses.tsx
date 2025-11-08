817508e1a10bae5dfe135ea6796de877fe2c53a6
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-11-08 13:35:56 +0500

    add a spotlight and moving-border

---
+++ src/pages/FeaturedCourses.tsx
@@ -0,0 +1,62 @@
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import courseData from "@/data/music_courses.json";
+import Link from "next/link";
+
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+}
+
+function FeaturedCourses() {
+  const featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+          <h2 className="text-base text-teal-600 font-semibold uppercase tracking-tight">
+            FEATURE COURSES
+          </h2>
+          <p className="tracking-tight text-white mt-2 leading-8 text-3xl font-extrabold">
+            Learn with the Best
+          </p>
+        </div>
+      </div>
+      <div className="mt-10 mx-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className="flex justify-center">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
+                </div>
+              </BackgroundGradient>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-20 text-center">
+        <Link
+          href="/courses"
+          className="mt-8 bg-teal-600 text-white py-2 px-4 rounded-md"
+        >
+          All Courses
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedCourses;
